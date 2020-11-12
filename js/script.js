0202dfc7d60899807f7d7f4369fe4222a6e8780f
Katrin <kostiukkaterina@gmail.com>
2020-11-12 14:01:55 +0200

    practice 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 "use strict";
 
-// alert('Hello!');
+const numberOfFilms = +prompt ("Сколько фильмов вы посмотрели?", "");
 
-const result = confirm("Are you here?");
-console.log(result);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ("Один из последниз просмотренных фильмов?", ""),
+    b = prompt ("На сколько оцените его?", ""),
+
+    c = prompt ("Один из последниз просмотренных фильмов?", ""),
+    d = prompt ("На сколько оцените его?", "");
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
+
+console.log(personalMovieDB);
